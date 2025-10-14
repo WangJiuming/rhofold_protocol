c3f266f3273797422f14838b710ece6e826e885d
WangJiuming <jmwang@link.cuhk.edu.hk>
2025-10-14 19:12:27 +0800

    Feat: Add search msa script without manual setup

---
+++ scripts/3_search_msa.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+fasta_path=$!
+
+db0_path="../msa_database/db/Rfam.cm"
+db1_path="../msa_database/db/rnacentral.fasta"
+db2_path="../msa_database/db/nt"
+db0to1_path="../msa_database/db/rfam_annotations.tsv.gz"
+db0to2_path="../msa_database/db/Rfam.full_region.gz"
+cpu=32
+
+echo "MSA search started."
+
+./rmsa/rMSA.pl "${fasta_path}" -db0="${db0_path}" \
+                               -db1="${db1_path}" \
+                               -db2="${db2_path}" \
+                               -db0to1="${db0to1_path}" \
+                               -db0to2="${db0to2_path}" \
+                               -cpu=$cpu
+
+echo "MSA search completed."
